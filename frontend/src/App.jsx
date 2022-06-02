d657199f08ea5deadfa180d97fd1b6866266e953
Oscar AGUDELO <oscar.agudelo.pro@gmail.com>
2022-06-02 23:50:05 +0200

    login front is ok, css colors must be changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "@pages/Home/Home";
+import Login from "@components/Login/Login";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Login />
       <Home />
       <p>coucou</p>
     </div>
