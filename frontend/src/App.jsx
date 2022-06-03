9c90accf3112f0960c4d80dd7609ea0359c9fd69
o.pochic@gmail.com <o.pochic@gmail.com>
2022-06-03 09:43:37 +0200

    Creation du formulaire dans CreationEvenement.jsx et CreationEvenement.css: envoi n°1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Home from "@pages/Home/Home";
-
+import CreationEvenement from "@components/CreationEvenement/CreationEvenement";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <CreationEvenement />
       <Home />
       <p>coucou</p>
     </div>
