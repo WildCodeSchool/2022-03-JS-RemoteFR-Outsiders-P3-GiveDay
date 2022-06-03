d794510a9aecc3c2e16204c692001a1367701ad9
o.pochic@gmail.com <o.pochic@gmail.com>
2022-06-03 11:25:31 +0200

    Modification du formulaire CreationEvenement.jsx : envoi n°2

--- frontend/src/components/CreationEvenement/CreationEvenement.jsx
+++ frontend/src/components/CreationEvenement/CreationEvenement.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import "./creationEvenement.css";
 
 function CreationEvenement() {
+  const handleChange = (event) => {
+    event.preventDefault();
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    console.warn(Array.from(data.entries()));
+  };
+
   return (
     <div>
       <h1>CreationEvenement</h1>
-      <form id="creationEvenement" action="/api/route/evenement" method="post">
+      <form
+        id="creationEvenement"
+        className="giveForm"
+        action="/api/route/evenement"
+        method="post"
+        onSubmit={onSubmit}
+      >
         <input
           id="input_eve_mail"
           type="mail"
+          onChange={handleChange}
           name="adresse_mail"
           pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
           placeholder="Entrez votre email exemple :john.doe@email.fr"
@@ -17,6 +34,7 @@ function CreationEvenement() {
 
         <input
           id="input_eve_lastname"
+          onChange={handleChange}
           type="text"
           name="lastname"
           placeholder="Entrez votre nom"
@@ -24,6 +42,7 @@ function CreationEvenement() {
         />
         <input
           id="input_eve_firstname"
+          onChange={handleChange}
           type="text"
           name="firstname"
           placeholder="Entrez votre prénom"
@@ -34,6 +53,7 @@ function CreationEvenement() {
           id="input_eve_date"
           type="date"
           name="date"
+          onChange={handleChange}
           value="2018-07-22"
           min="2018-01-01"
           max="2018-12-31"
@@ -43,6 +63,7 @@ function CreationEvenement() {
         <input
           id="input_eve_hour_start"
           type="time"
+          onChange={handleChange}
           name="heure_debut"
           min="09:00"
           max="22:00"
@@ -52,6 +73,7 @@ function CreationEvenement() {
         <input
           id="input_eve_hour_end"
           type="time"
+          onChange={handleChange}
           name="heure_fin"
           min="11:00"
           max="00:00"
@@ -60,6 +82,7 @@ function CreationEvenement() {
 
         <input
           id="input_eve_place"
+          onChange={handleChange}
           type="text"
           name="lieu_rendez_vous"
           placeholder="Lieu du rendez-vous"
@@ -69,6 +92,7 @@ function CreationEvenement() {
         <input
           id="input_eve_phone"
           type="tel"
+          onChange={handleChange}
           name="Numero_telephone"
           pattern="^(\+33 |0)[1-9]( \d\d){4}$"
           placeholder="Entrez votre numero exemple : 06 99 33 22 11"
