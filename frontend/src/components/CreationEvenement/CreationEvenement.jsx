3c7b0b18d8502d312c871fe6f1530390a76ebb8a
o.pochic@gmail.com <o.pochic@gmail.com>
2022-06-03 12:04:54 +0200

    Modification du formulaire CreationEvenement.jsx, ajout de labels aux input : envoi n°3

--- frontend/src/components/CreationEvenement/CreationEvenement.jsx
+++ frontend/src/components/CreationEvenement/CreationEvenement.jsx
@@ -22,94 +22,122 @@ function CreationEvenement() {
         method="post"
         onSubmit={onSubmit}
       >
-        <input
-          id="input_eve_mail"
-          type="mail"
-          onChange={handleChange}
-          name="adresse_mail"
-          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-          placeholder="Entrez votre email exemple :john.doe@email.fr"
-          required
-        />
+        <label htmlFor="input_eve_mail">
+          Email
+          <input
+            id="input_eve_mail"
+            type="mail"
+            onChange={handleChange}
+            name="adresse_mail"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            placeholder="Entrez votre email exemple :john.doe@email.fr"
+            required
+          />
+        </label>
 
-        <input
-          id="input_eve_lastname"
-          onChange={handleChange}
-          type="text"
-          name="lastname"
-          placeholder="Entrez votre nom"
-          required
-        />
-        <input
-          id="input_eve_firstname"
-          onChange={handleChange}
-          type="text"
-          name="firstname"
-          placeholder="Entrez votre prénom"
-          required
-        />
+        <label htmlFor="input_eve_lastname">
+          Nom
+          <input
+            id="input_eve_lastname"
+            onChange={handleChange}
+            type="text"
+            name="lastname"
+            placeholder="Entrez votre nom"
+            required
+          />
+        </label>
 
-        <input
-          id="input_eve_date"
-          type="date"
-          name="date"
-          onChange={handleChange}
-          value="2018-07-22"
-          min="2018-01-01"
-          max="2018-12-31"
-          required
-        />
+        <label htmlFor="input_eve_firstname">
+          Prénom
+          <input
+            id="input_eve_firstname"
+            onChange={handleChange}
+            type="text"
+            name="firstname"
+            placeholder="Entrez votre prénom"
+            required
+          />
+        </label>
+        <label htmlFor="input_eve_firstname">
+          Date de l'évènement
+          <input
+            id="input_eve_date"
+            type="date"
+            name="date"
+            onChange={handleChange}
+            value="2018-07-22"
+            min="2018-01-01"
+            max="2018-12-31"
+            required
+          />
+        </label>
+        <label htmlFor="input_eve_hour_start">
+          Heure de début
+          <input
+            id="input_eve_hour_start"
+            type="time"
+            onChange={handleChange}
+            name="heure_debut"
+            min="09:00"
+            max="22:00"
+            required
+          />
+        </label>
+        <label htmlFor="input_eve_hour_end">
+          Heure de fin
+          <input
+            id="input_eve_hour_end"
+            type="time"
+            onChange={handleChange}
+            name="heure_fin"
+            min="11:00"
+            max="00:00"
+            required
+          />
+        </label>
 
-        <input
-          id="input_eve_hour_start"
-          type="time"
-          onChange={handleChange}
-          name="heure_debut"
-          min="09:00"
-          max="22:00"
-          required
-        />
+        <label htmlFor="input_eve_place">
+          Lieu du rendez-vous
+          <input
+            id="input_eve_place"
+            onChange={handleChange}
+            type="text"
+            name="lieu_rendez_vous"
+            placeholder="Lieu du rendez-vous"
+            required
+          />
+        </label>
 
-        <input
-          id="input_eve_hour_end"
-          type="time"
-          onChange={handleChange}
-          name="heure_fin"
-          min="11:00"
-          max="00:00"
-          required
-        />
+        <label htmlFor="input_eve_phone">
+          N° Téléphone
+          <input
+            id="input_eve_phone"
+            type="tel"
+            onChange={handleChange}
+            name="Numero_telephone"
+            pattern="^(\+33 |0)[1-9]( \d\d){4}$"
+            placeholder="Entrez votre numero exemple : 06 99 33 22 11"
+            required
+          />
+        </label>
 
-        <input
-          id="input_eve_place"
-          onChange={handleChange}
-          type="text"
-          name="lieu_rendez_vous"
-          placeholder="Lieu du rendez-vous"
-          required
-        />
+        <label htmlFor="input_eve_present">
+          Vos cadeaux
+          <textarea id="input_eve_present" name="cadeaux" rows="5">
+            entrez vos souhaits de cadeaux
+          </textarea>
+        </label>
 
-        <input
-          id="input_eve_phone"
-          type="tel"
-          onChange={handleChange}
-          name="Numero_telephone"
-          pattern="^(\+33 |0)[1-9]( \d\d){4}$"
-          placeholder="Entrez votre numero exemple : 06 99 33 22 11"
-          required
-        />
-
-        <textarea id="input_eve_present" name="cadeaux" rows="5">
-          entrez vos souhaits de cadeaux
-        </textarea>
-
-        <select id="select_eve_theme" name="theme">
-          <option value="1" selected="selected">
-            Panda
-          </option>
-          <option value="2">starwars</option>
-          <option value="3">chats</option>
-        </select>
+        <label htmlFor="select_eve_theme">
+          Le thème de la carte
+          <select id="select_eve_theme" name="theme">
+            <option value="1" selected="selected">
+              Panda
+            </option>
+            <option value="2">starwars</option>
+            <option value="3">chats</option>
+          </select>
+        </label>
 
         <input type="submit" value="Envoyer le formulaire" />
       </form>
