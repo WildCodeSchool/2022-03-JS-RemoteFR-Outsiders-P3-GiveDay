50898e8db6104ed835040e0e3a92dcc6c59db063
Valerie <valerie.ielsch@gmail.com>
2022-06-07 18:28:54 +0200

    carousel fonctionnel, pas de version définitive , pas encore responsive mis en commentaire pour passer les test eslint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Home from "@pages/Home/Home";
+import Associations from "@pages/Associations/Associations";
 
 import "./App.css";
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      <Associations />
     </div>
   );
 }
