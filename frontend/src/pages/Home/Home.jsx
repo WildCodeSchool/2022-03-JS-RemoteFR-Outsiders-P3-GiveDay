fae885d36cf9dfd31c12e7f49a28577aa535a88a
Valerie <valerie.ielsch@gmail.com>
2022-06-10 10:37:13 +0200

    desinstaller axios, react router dom cors du dossier  source

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,11 +5,6 @@ import CreationEvenement from "@components/CreationEvenement/CreationEvenement";
 // import Userback from "@components/AxiosBack/Userback"
 // import Eventback from "@components/AxiosBack/Eventback"
 
-
-
-
-
-
 function Home() {
   return (
     <div>
