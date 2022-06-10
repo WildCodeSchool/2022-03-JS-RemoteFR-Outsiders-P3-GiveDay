76c6e69114d4653731077abbaa39be9a4ce38690
ALI BELALA <alouch74160@gmail.com>
2022-06-10 15:41:22 +0200

    creation du footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import "./assets/root.css";
 import "./App.css";
 import Header from "@components/Header/Header";
+import { Routes, Route } from "react-router-dom";
+import Contact from "@components/Contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <div className="App">
+        <Header />
+      </div>
+      <div>
+        <Routes>
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
