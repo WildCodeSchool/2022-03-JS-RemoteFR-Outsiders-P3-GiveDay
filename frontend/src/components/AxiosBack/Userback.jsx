1ae3ab73c3a5e4ed574152abc568759002be3eb6
AnthonyGorski <38192276+GorskiAnthony@users.noreply.github.com>
2022-06-14 10:44:27 +0200

    Update Userback.jsx

--- frontend/src/components/AxiosBack/Userback.jsx
+++ frontend/src/components/AxiosBack/Userback.jsx
@@ -1,26 +1,25 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-// function Tempback() {
-//   const [user, setuser] = useState("");
-//   const API = `http://localhost:5000/api/user`;
-//   useEffect(() => {
-//     axios
-//       .get(API)
-//       .then((res) => res.data)
-//       .then((data) => {
-//         setuser(data);
-//       })
-//       .catch((e) => console.error(e));
-//   }, []);
+function Userback() {
+  const [user, setuser] = useState("");
+  const API = `http://localhost:5000/api/user`;
+  useEffect(() => {
+    axios
+      .get(API)
+      .then((res) => res.data)
+      .then((data) => {
+        setuser(data);
+      })
+      .catch((e) => console.error(e));
+  }, []);
 
-//   return (
-//     <p>
-//       coucou
-//       {/* test de user */}
-//       {console.warn(user)}
-//     </p>
-//   );
-// }
+  return (
+    <p>
+      {/* test de user */}
+      {console.warn(user)}
+    </p>
+  );
+}
 
-// export default Tempback;
+export default Userback;
