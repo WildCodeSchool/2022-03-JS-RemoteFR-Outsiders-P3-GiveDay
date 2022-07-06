06b9ef78e40b49a5516ce10ba5eb62371e3f8a77
Valerie <valerie.ielsch@gmail.com>
2022-07-06 14:52:39 +0200

    add cagnotte in table event and delete cagnotte table + add comment on router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -62,6 +62,13 @@ router.put("/api/article/:id", ArticleController.edit);
 router.post("/api/article", ArticleController.add);
 router.delete("/api/article/:id", ArticleController.delete);
 
+/**
+ * @desc Users routes
+ *
+ * @api {get} /api/users Récupère tous les utilisateurs
+ * @api {delete} /api/users/delete/:id Supprime un utilisateur de la base de données
+
+ */
 router.get("/api/users", UserController.browse);
 router.delete("/api/users/delete/:id", UserController.delete);
 module.exports = router;
