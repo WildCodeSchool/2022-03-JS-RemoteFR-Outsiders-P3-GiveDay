3b6b499c8b58575c67775b3bf65bd46994951ff7
Valerie <valerie.ielsch@gmail.com>
2022-07-23 18:21:48 +0200

    add alerts messages on dashboard buttons and modified css

---
+++ frontend/src/pages/dashboard/Admin/AccueilContent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AccueilContent() {
+  return (
+    <div className="contentTable">
+      <div className="head-h2">
+        <h2>Bienvenue sur le portail administrateur</h2>
+      </div>
+      <h4>
+        Ici, vous pourrez gérer vos utilisateurs : modifier leur rôle ou
+        supprimer l'utilisateur, et gérer vos articles: créer un article et
+        supprimer un article.
+      </h4>
+    </div>
+  );
+}
+
+export default AccueilContent;
--- frontend/src/pages/dashboard/Admin/Admin.jsx
+++ frontend/src/pages/dashboard/Admin/Admin.jsx
@@ -4,6 +4,7 @@ import CurrentPagesContext from "../../../PagesContexts";
 import UsersContent from "../Users/UsersContent";
 import PostsContent from "../Posts/PostsContents";
 import CreatePostContent from "../Posts/CreatePostContent";
+import AccueilContent from "./AccueilContent";
 import Edit from "../Users/Edit";
 import "../dashboard.css";
 
@@ -17,20 +18,25 @@ function Admin({ content }) {
     return (
       <div className="adminPage">
         <nav id="adminNav">
-          <h1>GiveDay</h1>
+          <Link to="/admin">
+            {" "}
+            <h1>GiveDay</h1>
+          </Link>
+
           <ul>
             <li>
-              <Link to="/admin/users">Users</Link>
+              <Link to="/admin/users">Utilisateurs</Link>
             </li>
             <li>
-              <Link to="/admin/posts">Posts</Link>
+              <Link to="/admin/posts">Blog</Link>
             </li>
             <li>
-              <Link to="/admin/createPost">Créer un Post</Link>
+              <Link to="/admin/createPost">Créer un article</Link>
             </li>
           </ul>
         </nav>
         <div className="adminContent">
+          {content === "accueil" ? <AccueilContent /> : null}
           {content === "users" ? <UsersContent /> : null}
           {content === "posts" ? <PostsContent /> : null}
           {content === "createPost" ? <CreatePostContent /> : null}
